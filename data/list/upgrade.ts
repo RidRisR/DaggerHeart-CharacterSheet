98331d24b1bb44eef0dc273acc7ec48a5bb470b0
RidRisR <79858083+RidRisR@users.noreply.github.com>
2025-05-27 18:16:40 +0800

    mod upgrade

--- data/list/upgrade.ts
+++ data/list/upgrade.ts
@@ -7,7 +7,7 @@ export const upgradeOptionsData = {
     { label: "永久增加一个压力槽。", doubleBox: false, boxCount: 1 },
     { label: "选择两项经历获得额外+1。", doubleBox: false, boxCount: 1 },
     { label: "选择一张不高于你当前等级的领域卡加入卡组。", doubleBox: false, boxCount: 1 },
-    { label: "闪避值获得+1。", doubleBox: false, boxCount: 1 },
+    { label: "获得闪避值+1。", doubleBox: false, boxCount: 1 },
   ],
 
   // 特定等级升级选项
@@ -15,14 +15,14 @@ export const upgradeOptionsData = {
     tier1: [
     ],
     tier2: [
-      { label: "升级你的子职业，你不可再使用T3级别升级选项中的“兼职”选项。", doubleBox: false, boxCount: 1 },
-      { label: "熟练度获得+1。", doubleBox: true, boxCount: 2 },
-      { label: "获得一个额外的子职业，你不可再使用T3级别升级选项中的“升级子职业”选项。也不可再使用T3或更高级别的“兼职”选项。", doubleBox: true, boxCount: 2 },
+      { label: "升级你的子职业，你不可再使用T3级别的“兼职”选项。", doubleBox: false, boxCount: 1 },
+      { label: "获得熟练度+1。", doubleBox: true, boxCount: 2 },
+      { label: "兼职：获得一个额外的职业、子职业和一个领域。你不可再使用T3级别的“升级子职业”选项。也不可使用其他任何“兼职”选项。", doubleBox: true, boxCount: 2 },
     ],
     tier3: [
-      { label: "升级你的子职业，你不可再使用T4级别升级选项中的“兼职”选项。", doubleBox: false, boxCount: 1 },
-      { label: "熟练度获得+1。", doubleBox: true, boxCount: 2 },
-      { label: "获得一个额外的子职业，你不可再使用T4级别升级选项中的“升级子职业”选项。也不可再使用T4或更高级别的“兼职”选项。", doubleBox: true, boxCount: 2 },
+      { label: "升级你的子职业，你不可再使用T4级别的“兼职”选项。", doubleBox: false, boxCount: 1 },
+      { label: "获得熟练度+1。", doubleBox: true, boxCount: 2 },
+      { label: "兼职：获得一个额外的职业、子职业和一个领域。你不可再使用T4级别的“升级子职业”选项。也不可使用其他任何“兼职”选项。", doubleBox: true, boxCount: 2 },
     ],
   },
 };

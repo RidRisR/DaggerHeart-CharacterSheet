b6c107baee98349e3a28afd03749d61a792e76d9
RidRisR <79858083+RidRisR@users.noreply.github.com>
2025-05-27 12:56:46 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -6,11 +8,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  basePath: isProd ? '/DaggerHeart-CharacterSheet' : '',
+  assetPrefix: isProd ? '/DaggerHeart-CharacterSheet/' : '',
   images: {
     unoptimized: true,
   },
   output: 'export',
-  assetPrefix: './', // 添加这一行
 };
 
 export default nextConfig;

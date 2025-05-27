fd67620f3fe51d3c1cc87cd6ff66a860fd95fa9e
RidRisR <79858083+RidRisR@users.noreply.github.com>
2025-05-27 14:04:12 +0800

    fix local build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+
 
 const nextConfig = {
   eslint: {
@@ -8,8 +8,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  basePath: isProd ? '/DaggerHeart-CharacterSheet' : '',
-  assetPrefix: isProd ? '/DaggerHeart-CharacterSheet/' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '.',
   images: {
     unoptimized: true,
   },

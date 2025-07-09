b6ce53a24ac3416100dfc91074cc3cc44c5f6220
ridrisr <79858083+RidRisR@users.noreply.github.com>
2025-07-09 23:41:18 +0800

    min height of hope trait

--- components/character-sheet-sections/hope-section.tsx
+++ components/character-sheet-sections/hope-section.tsx
@@ -41,7 +41,7 @@ export function HopeSection() {
       </div>
 
       <div className="text-center px-2">
-        <div className="text-[12px] leading-tight">
+        <div className="text-[12px] leading-tight min-h-[2em]">
           <ReactMarkdown>{hopeTrait}</ReactMarkdown>
         </div>
       </div>

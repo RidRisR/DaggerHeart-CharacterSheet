aef6fd4bb5f552917602dc8c0662de570a3fbbc4
ridrisr <79858083+RidRisR@users.noreply.github.com>
2025-07-09 23:33:26 +0800

    fix upgrade

--- data/list/upgrade.ts
+++ data/list/upgrade.ts
@@ -3,8 +3,8 @@ export const upgradeOptionsData = {
   // 基础升级选项（所有职业通用）
   baseUpgrades: [
     { label: "两项未升级的角色属性+1，然后将该属性标记为已升级。", doubleBox: false, boxCount: 3 },
-    { label: "永久增加一个生命槽。", doubleBox: false, boxCount: 1 },
-    { label: "永久增加一个压力槽。", doubleBox: false, boxCount: 1 },
+    { label: "永久增加一个生命槽。", doubleBox: false, boxCount: 2 },
+    { label: "永久增加一个压力槽。", doubleBox: false, boxCount: 2 },
     { label: "选择两项经历获得额外+1。", doubleBox: false, boxCount: 1 },
     { label: "选择一张不高于你当前等级的领域卡加入卡组。", doubleBox: false, boxCount: 1 },
     { label: "获得闪避值+1。", doubleBox: false, boxCount: 1 },

e9f5c8a66dcd62268ded23d638db89cdda460895
ridrisr <79858083+RidRisR@users.noreply.github.com>
2025-07-11 23:20:03 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '.',
   images: {
     unoptimized: true,
   },
+  output: 'export',
 };
 
 export default nextConfig;

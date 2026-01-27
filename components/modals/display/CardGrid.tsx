a1bb0b7a6f69572198c1cccccc20ff765d763782
ridrisr <79858083+RidRisR@users.noreply.github.com>
2026-01-27 18:47:41 +0800

    justify card to center

--- components/modals/display/CardGrid.tsx
+++ components/modals/display/CardGrid.tsx
@@ -26,7 +26,7 @@ export function CardGrid<T extends StandardCard | ExtendedStandardCard>({
   return (
     <div
       className={cn(
-        "grid gap-4",
+        "grid gap-4 justify-items-center",
         isTextMode
           ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
           : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3",
